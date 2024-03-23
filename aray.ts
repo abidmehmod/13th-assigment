1a4bc4e41fb307dd98950af6a6bf70b042b93639
abidmehmod <160239077+abidmehmod@users.noreply.github.com>
2024-03-23 19:46:48 +0500

    own array

---
+++ aray.ts
@@ -0,0 +1,6 @@
+let transportation : string [] = ['bike','car','suzuki','caltus'];
+for(let i = 0; i<transportation.length; i++){
+
+    console.log('I would like to own a : ' + transportation[i]);
+
+}
